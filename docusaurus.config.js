d59ffe9e6fe36e5ba3548d18b771f236964d5002
BekahHW <34313413+BekahHW@users.noreply.github.com>
2024-06-14 15:43:17 -0400

    Change url to point to os.pizza (#228)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://sauced-intro.netlify.app/',
+  url: 'https://opensauced.pizza',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/learn/',
