3318f15523245b083209c5d2a15eb45eef97a1fc
Bekah Hawrot Weigel <rhawrot@gmail.com>
2024-06-14 14:27:28 -0400

    Remove comment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,6 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  // url: 'https://intro.opensauced.pizza',
   url: 'https://sauced-intro.netlify.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
