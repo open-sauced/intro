d5054b71766928224b71d119d4c46ed4b82f63f9
Chaitanya.K <132165121+Codexnever@users.noreply.github.com>
2024-10-04 15:02:26 +0530

    Error:Your Docusaurus site did not load properly

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   url: 'https://opensauced.pizza',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/learn/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
